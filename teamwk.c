20dd459000fb869ee5b97f1537b09f4138f4698d
olaosebikan pelumi <oolaosebikan360@gmail.com>
2023-04-26 18:55:14 -0700

    simple shell

--- teamwk.c
+++ teamwk.c
@@ -8,14 +8,15 @@
 void teamwk(char **agrv, char **env)
 {
 	char *ptr = NULL;
-	int k, status;
+	int k, p,  status;
 	size_t n = 0;
 	ssize_t numc;
-	char *args[] = {NULL, NULL};
+	char *args[max_commad];
 	pid_t chd_pid;
 
-	while(1)
+	while (1)
 	{
+	if (isatty(STDIN_FILENO))
 	printf("desshell$ ");
 	numc = getline(&ptr, &n, stdin);
 	if (numc == -1)
@@ -24,13 +25,18 @@ void teamwk(char **agrv, char **env)
 	exit(EXIT_FAILURE);
 	}
 	k = 0;
-	while(ptr[k])
+	while (ptr[k])
 	{
 	if (ptr[k] == '\n')
 	ptr[k] = 0;
 	k++;
 	}
-	args[0] = ptr;
+	p = 0;
+	args[0] = strtok(ptr, ' ');
+	while (args[p])
+	{
+		arrgs[++p] = strtok(NULL, " ");
+	}
 	chd_pid = fork();
 	if (chd_pid == -1)
 	{
@@ -39,10 +45,10 @@ void teamwk(char **agrv, char **env)
 	}
 	if (chd_pid == 0)
 	{
-		if (execve(args[0], args, env) == -1)
-		printf("%s: no such file\n", agrv[0]);
+	if (execve(args[0], args, env) == -1)
+	printf("%s: no such file\n", agrv[0]);
 	}
 	else
-	wait(&status); 
-	}
+	wait(&status);
+}
 }
