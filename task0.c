4b278c8e3f05538b0b50a5ad87175cbd7619858c
olaosebikan pelumi <oolaosebikan360@gmail.com>
2023-04-28 16:05:22 -0700

    firstproject

---
+++ task0.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * sum_num - function that sums
+ * @x:variable 1
+ * @y: variable 2
+ *Return: sum
+ */
+
+
+
+int sum_num(int x, int y)
+{
+	int sum;
+
+	sum = x / y;
+	return (sum);
+}
